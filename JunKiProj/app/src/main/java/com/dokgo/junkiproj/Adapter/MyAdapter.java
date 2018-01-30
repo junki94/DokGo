6f3f000ca51af4f16c57dee7ede1d94bf6c10238
JawsGeun <whtnrms2012@naver.com>
2018-01-30 13:06:45 +0900

    기본 구조 만듬

---
+++ JunKiProj/app/src/main/java/com/dokgo/junkiproj/Adapter/MyAdapter.java
@@ -0,0 +1,8 @@
+package com.dokgo.junkiproj.Adapter;
+
+/**
+ * Created by JawsGeun on 2018-01-30.
+ */
+
+public class MyAdapter {
+}
---
+++ JunKiProj/app/src/main/java/com/dokgo/junkiproj/DetailActivity.java
@@ -0,0 +1,8 @@
+package com.dokgo.junkiproj;
+
+/**
+ * Created by JawsGeun on 2018-01-30.
+ */
+
+public class DetailActivity {
+}
